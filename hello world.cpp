b4927dc95cb8e6c585c9baced87cb3281b67a883
anirams <marina.stvoric1@gmail.com>
2018-11-27 09:29:13 +0100

    Hello world
    added first file

---
+++ hello world.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //ukljucuje neki library
+
+using namespace std; //da ne moramo pisat stalno std
+
+int main() { 
+	cout << "Hello World!" << endl; //izbacuje hello world, endl prebacuje u novi red 
+	system("PAUSE"); //zaustavi da mozemo vidjet sto smo pisali, samo VS
+	return 0; //sve je u redu
+}
