9911f79a3ddbf171c31c9acfb7594a08fc9b913a
anirams <marina.stvoric1@gmail.com>
2018-11-27 11:11:11 +0100

    3 new lines

--- hello world.cpp
+++ hello world.cpp
@@ -6,6 +6,9 @@ int main() {
 	cout << "Hello World!" << endl; //izbacuje hello world, endl prebacuje u novi red 
 	cout << "this is another sentece";
 	cout << 2;
+	cout << 3;
+	cout << 4;
+	cout << 5;
 	system("PAUSE"); //zaustavi da mozemo vidjet sto smo pisali, samo VS
 	return 0; //sve je u redu
 }
