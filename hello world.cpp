26b74a23690486a40ffc54aa63d26e418d1091b7
anirams <marina.stvoric1@gmail.com>
2018-11-27 09:35:30 +0100

    one line
    added one line of code

--- hello world.cpp
+++ hello world.cpp
@@ -4,6 +4,7 @@ using namespace std; //da ne moramo pisat stalno std
 
 int main() { 
 	cout << "Hello World!" << endl; //izbacuje hello world, endl prebacuje u novi red 
+	cout << "this is a sentence.";
 	system("PAUSE"); //zaustavi da mozemo vidjet sto smo pisali, samo VS
 	return 0; //sve je u redu
 }
